99282eb35c37d8287805fde005b5bd31ac24ca65
DavidOnofre <88davido@gmail.com>
2021-03-05 13:05:48 -0500

    filtro varios

--- js/scripts.js
+++ js/scripts.js
@@ -372,8 +372,8 @@ function list(array_list) {
 }
 
 function myFunction() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
+    // Declare variables 
+    var input, filter, table, tr, td, i, j, visible;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
@@ -381,14 +381,18 @@ function myFunction() {
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+      visible = false;
+      /* Obtenemos todas las celdas de la fila, no sólo la primera */
+      td = tr[i].getElementsByTagName("td");
+      for (j = 0; j < td.length; j++) {
+        if (td[j] && td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+          visible = true;
         }
+      }
+      if (visible === true) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
       }
     }
   }
